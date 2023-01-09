ec14402c0381b6bcfd9e0acf207366c62b472a33
viktorkucherov <viktorkucherov@outlook.com>
2023-01-10 00:54:51 +0300

    Prolog added

--- csv_parser/views.py
+++ csv_parser/views.py
@@ -14,11 +14,16 @@ class IndexView(TemplateView):
 
     def post(self, request):
 
-        tmp_csv: TextIO = request.FILES['csv']
-        decoded_file = tmp_csv.read().splitlines()
-        dialect = Dialect()
-        dialect.delimiter = str(';')
-        reader = csv.DictReader(decoded_file, dialect=dialect)
+        tmp_csv = request.FILES['csv']
+        tmp_csv = tmp_csv.read()
+        with open(tmp_csv) as f:
+
+            print(f)
+
+        # decoded_file = tmp_csv.read().splitlines()
+        # dialect = Dialect()
+        # dialect.delimiter = str(';')
+        # reader = csv.DictReader(decoded_file, dialect=dialect)
 
         return render(request=request, template_name=self.template_name)
 
--- djangoCSVParser/urls.py
+++ djangoCSVParser/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from csv_parser.views import IndexView
+from prolog_module.views import prolog
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
+    path('index/', IndexView.as_view(), name='index'),
+    path('', prolog, name='prolog')
 ]
